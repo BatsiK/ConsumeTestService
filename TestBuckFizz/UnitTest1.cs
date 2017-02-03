44d9f26e7e4374635bf5a55a774dec086daa8288
BatsiK <brkahwenga@gmail.com>
2017-02-03 19:17:08 +0200

    Add files via upload

---
+++ TestBuckFizz/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.IO;
+using ConsumeRESTFulService;
+
+namespace TestRESTFulService
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestService()
+        {
+            // arrange  
+            string input ="Go Seamless";
+            string output= "Go Seamless";
+            ConsumeService service = new ConsumeService();
+
+            // act  
+           output= service.RESTConsume(input);
+
+            // assert  
+            Assert.AreEqual(input, output, true);
+        }
+    }
+}
